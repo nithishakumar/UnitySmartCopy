a2e5ade68e9a521c49a352ca796b5345809d725e
nithishakumar <nithishananthakumar.tinie21@gmail.com>
2026-02-06 09:53:09 -0800

    add logic to paste values by name

--- UnitySmartCopy/Assets/Editor/SmartCopy.cs
+++ UnitySmartCopy/Assets/Editor/SmartCopy.cs
@@ -5,34 +5,51 @@ namespace Editor
 {
     public class SmartCopy
     {
-        private static SerializedObject sourceSnapshot;
+        private static SerializedObject clipboardSO;
         
         [MenuItem("CONTEXT/Component/Copy Component Values By Name")]
         private static void CopyComponentValuesByName(MenuCommand command)
         {
             // The component that was right-clicked
             Component comp = (Component)command.context;
-
-            // Example action: log the component type and GameObject
-            Debug.Log($"Clicked menu on {comp.GetType().Name} attached to {comp.gameObject.name}");
             
-            sourceSnapshot = new SerializedObject(comp);
+            // Clone the object and store serialized data in editor memory
+            clipboardSO = new SerializedObject(Object.Instantiate(comp));
         }
         
         [MenuItem("CONTEXT/Component/Paste Component Values By Name")]
         private static void PasteValuesByName(MenuCommand command)
         {
-            // The component that was right-clicked
-            Component comp = (Component)command.context;
+            var clickedComp = (Component)command.context;
+            var clickedSO = new SerializedObject(clickedComp);
+            var enterChildren = true;
+            var clickedProp = clickedSO.GetIterator();
 
-            // Example action: log the component type and GameObject
-            Debug.Log($"Clicked menu on {comp.GetType().Name} attached to {comp.gameObject.name}");
+            while (clickedProp.NextVisible(enterChildren))
+            {
+                enterChildren = false;
+                
+                // Try to find a matching property in the CLIPBOARD by name
+                var clipboardProp = clipboardSO.FindProperty(clickedProp.name);
+                
+                // TODO: note down properties that were copied and not copied and give a summary to the user
+                
+                // Check if it exists AND the types match (e.g., don't paste a string into a float)
+                if (clipboardProp != null && clipboardProp.propertyType == clickedProp.propertyType)
+                {
+                    clickedSO.CopyFromSerializedProperty(clipboardProp);
+                }
+                
+            }
+            
+            clickedSO.ApplyModifiedProperties();
+            Debug.Log($"Smart Pasted values into {clickedComp.GetType().Name} on {clickedComp.gameObject.name}");
         }
         
         [MenuItem("CONTEXT/Component/Paste Component Values By Name", true)]
         private static bool ValidatePasteValuesByName(MenuCommand command)
         {
-            return sourceSnapshot != null;
+            return clipboardSO != null;
         }
     }
 }
