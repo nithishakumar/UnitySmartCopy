edc1b89df0770b8facbc6c4814ab2ead2737b837
nithishakumar <nithishananthakumar.tinie21@gmail.com>
2026-02-07 15:48:43 -0800

    change logic to copy and paste values

--- UnitySmartCopy/Assets/Editor/SmartCopy.cs
+++ UnitySmartCopy/Assets/Editor/SmartCopy.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,51 +6,72 @@ namespace Editor
 {
     public class SmartCopy
     {
-        private static SerializedObject clipboardSO;
+        struct SerializedPropertyData
+        {
+            public SerializedPropertyType propertyType;
+            public GlobalObjectId globalObjectId;
+            public object boxedValue;
+        }
+        private static Dictionary<string, SerializedPropertyData> clipboard = new();
         
         [MenuItem("CONTEXT/Component/Copy Component Values By Name")]
         private static void CopyComponentValuesByName(MenuCommand command)
         {
             // The component that was right-clicked
-            Component comp = (Component)command.context;
+            var comp = (Component)command.context;
             
-            // Clone the object and store serialized data in editor memory
-            clipboardSO = new SerializedObject(Object.Instantiate(comp));
+            clipboard.Clear();
+            
+            var iterator = new SerializedObject(comp).GetIterator();
+            while (iterator.NextVisible(true))
+            {
+                if (iterator.name == "m_Script") continue;
+                var entry = new SerializedPropertyData
+                {
+                    propertyType = iterator.propertyType
+                };
+                if (iterator.propertyType == SerializedPropertyType.ObjectReference)
+                {
+                    entry.globalObjectId = GlobalObjectId.GetGlobalObjectIdSlow(iterator.objectReferenceValue);
+                }
+                else
+                {
+                    entry.boxedValue = iterator.boxedValue;
+                }
+                clipboard.Add(iterator.propertyPath, entry);
+            }
         }
         
         [MenuItem("CONTEXT/Component/Paste Component Values By Name")]
         private static void PasteValuesByName(MenuCommand command)
         {
             var clickedComp = (Component)command.context;
-            var clickedSO = new SerializedObject(clickedComp);
-            var enterChildren = true;
-            var clickedProp = clickedSO.GetIterator();
-
-            while (clickedProp.NextVisible(enterChildren))
+            var targetSO = new SerializedObject(clickedComp);
+            foreach (var kvp in clipboard)
             {
-                enterChildren = false;
-                
-                // Try to find a matching property in the CLIPBOARD by name
-                var clipboardProp = clipboardSO.FindProperty(clickedProp.name);
-                
-                // TODO: note down properties that were copied and not copied and give a summary to the user
-                
-                // Check if it exists AND the types match (e.g., don't paste a string into a float)
-                if (clipboardProp != null && clipboardProp.propertyType == clickedProp.propertyType)
+                var path = kvp.Key;
+                var data = kvp.Value;
+                var targetProp = targetSO.FindProperty(path);
+                if (targetProp == null || targetProp.propertyType != data.propertyType)
                 {
-                    clickedSO.CopyFromSerializedProperty(clipboardProp);
+                    continue;
                 }
-                
+                if (targetProp.propertyType == SerializedPropertyType.ObjectReference)
+                {
+                    targetProp.objectReferenceValue = GlobalObjectId.GlobalObjectIdentifierToObjectSlow(data.globalObjectId);
+                }
+                else
+                {
+                    targetProp.boxedValue = data.boxedValue;
+                }
             }
-            
-            clickedSO.ApplyModifiedProperties();
-            Debug.Log($"Smart Pasted values into {clickedComp.GetType().Name} on {clickedComp.gameObject.name}");
+            targetSO.ApplyModifiedProperties();
         }
         
         [MenuItem("CONTEXT/Component/Paste Component Values By Name", true)]
         private static bool ValidatePasteValuesByName(MenuCommand command)
         {
-            return clipboardSO != null;
+            return clipboard != null && clipboard.Count > 0;
         }
     }
 }
---
+++ UnitySmartCopy/Assets/Test.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obj1;
+
+    [SerializeField] 
+    private string string1;
+    
+    [SerializeField]
+    private Color color1;
+    
+    [SerializeField]
+    private int int1;
+    
+    [SerializeField]
+    private float float1;
+
+    [SerializeField] 
+    private Vector3 vector3;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
