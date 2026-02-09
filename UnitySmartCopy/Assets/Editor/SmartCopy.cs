56e99e85301f5811bbe5e9b6d8a6a1bafb1f76c9
nithishakumar <nithishananthakumar.tinie21@gmail.com>
2026-02-09 15:14:56 -0800

    add json processing

--- UnitySmartCopy/Assets/Editor/SmartCopy.cs
+++ UnitySmartCopy/Assets/Editor/SmartCopy.cs
@@ -1,74 +1,133 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Unity.Plastic.Newtonsoft.Json;
 using UnityEditor;
-using UnityEngine;
+using Unity.Plastic.Newtonsoft.Json.Linq;
 
-public class SmartCopy 
+public class SmartCopy
+{
+    private static readonly HashSet<string> PUBLIC_PROPERTIES_TO_IGNORE = new() { "m_Script" };
+
+    private static JObject sourceJsonObject;
+    private static HashSet<string> validPublicProperties = new();
+    private static Dictionary<string, SerializedPropertyType> expectedPropertyTypes = new();
+    private static Dictionary<string, int> objectReferenceValues = new();
+
+    [MenuItem("CONTEXT/Component/Copy Serialized Values")]
+    private static void CopySerializedValues(MenuCommand command)
     {
-        struct SerializedPropertyData
+        objectReferenceValues.Clear();
+        expectedPropertyTypes.Clear();
+
+        var sourceObject = command.context;
+        var serializedSourceObject = new SerializedObject(sourceObject);
+        validPublicProperties = GetValidPublicProperties(serializedSourceObject);
+        var sourceJson = EditorJsonUtility.ToJson(sourceObject);
+        sourceJsonObject = JObject.Parse(sourceJson);
+        foreach (var property in validPublicProperties)
         {
-            public SerializedPropertyType propertyType;
-            //public GlobalObjectId globalObjectId;
-            public object boxedValue;
-        }
-        private static Dictionary<string, SerializedPropertyData> clipboard = new();
-        
-        [MenuItem("CONTEXT/Component/Copy Component Values By Name")]
-        private static void CopyComponentValuesByName(MenuCommand command)
-        {
-            // The component that was right-clicked
-            var comp = (Component)command.context;
-            
-            clipboard.Clear();
-            
-            var iterator = new SerializedObject(comp).GetIterator();
-            while (iterator.NextVisible(true))
+            var serializedProp = serializedSourceObject.FindProperty(property);
+            if (serializedProp == null)
             {
-                if (iterator.name == "m_Script") continue;
-                var entry = new SerializedPropertyData
-                {
-                    propertyType = iterator.propertyType
-                };
-                /*if (iterator.propertyType == SerializedPropertyType.ObjectReference)
-                {
-                    entry.globalObjectId = GlobalObjectId.GetGlobalObjectIdSlow(iterator.objectReferenceValue);
-                }
-                else
-                {*/
-                    entry.boxedValue = iterator.boxedValue;
-                //}
-                clipboard.Add(iterator.propertyPath, entry);
+                continue;
             }
-        }
-        
-        [MenuItem("CONTEXT/Component/Paste Component Values By Name")]
-        private static void PasteValuesByName(MenuCommand command)
-        {
-            var clickedComp = (Component)command.context;
-            var targetSO = new SerializedObject(clickedComp);
-            foreach (var kvp in clipboard)
+
+            expectedPropertyTypes.Add(property, serializedProp.propertyType);
+            if (serializedProp.propertyType == SerializedPropertyType.ObjectReference)
             {
-                var path = kvp.Key;
-                var data = kvp.Value;
-                var targetProp = targetSO.FindProperty(path);
-                if (targetProp == null || targetProp.propertyType != data.propertyType)
-                {
-                    continue;
-                }
-                /*if (targetProp.propertyType == SerializedPropertyType.ObjectReference)
-                {
-                    targetProp.objectReferenceValue = GlobalObjectId.GlobalObjectIdentifierToObjectSlow(data.globalObjectId);
-                }
-                else
-                {*/
-                    targetProp.boxedValue = data.boxedValue;
-                //}
+                objectReferenceValues.Add(property, serializedProp.objectReferenceInstanceIDValue);
             }
-            targetSO.ApplyModifiedProperties();
-        }
-        
-        [MenuItem("CONTEXT/Component/Paste Component Values By Name", true)]
-        private static bool ValidatePasteValuesByName(MenuCommand command)
-        {
-            return clipboard != null && clipboard.Count > 0;
         }
     }
+
+    [MenuItem("CONTEXT/Component/Paste Serialized Values")]
+    private static void PasteValuesByPropertyPath(MenuCommand command)
+    {
+        var target = command.context;
+        var targetJson = EditorJsonUtility.ToJson(target);
+        var targetJsonObj = JObject.Parse(targetJson);
+        var targetJsonObjValue = targetJsonObj.Properties().First().Value;
+        var sourceJsonObjectValue = sourceJsonObject.Properties().First().Value;
+        var targetObj = new SerializedObject(target);
+        foreach (var property in validPublicProperties)
+        {
+            var serializedProp = targetObj.FindProperty(property);
+            if (serializedProp == null || serializedProp.propertyType == SerializedPropertyType.ObjectReference ||
+                serializedProp.propertyType != expectedPropertyTypes[property])
+            {
+                continue;
+            }
+
+            targetJsonObjValue[property] = sourceJsonObjectValue[property];
+        }
+
+        EditorJsonUtility.FromJsonOverwrite(targetJsonObj.ToString(Formatting.None), target);
+        targetObj = new SerializedObject(target);
+        foreach (var kvp in objectReferenceValues)
+        {
+            var serializedProp = targetObj.FindProperty(kvp.Key);
+            if (serializedProp != null && serializedProp.propertyType == SerializedPropertyType.ObjectReference)
+            {
+                serializedProp.objectReferenceInstanceIDValue = kvp.Value;
+            }
+        }
+
+        targetObj.ApplyModifiedProperties();
+    }
+
+    [MenuItem("CONTEXT/Component/Paste Serialized Values", true)]
+    private static bool ValidatePasteValuesByName(MenuCommand command)
+    {
+        return sourceJsonObject?.Properties().Any() == true;
+    }
+
+    // https://discussions.unity.com/t/serializedproperty-nextvisible-doesnt-work-with-hideininspector/801420/2
+    private delegate FieldInfo FieldInfoGetter(SerializedProperty p, out Type t);
+
+    private static HashSet<string> GetValidPublicProperties(SerializedObject serializedObject)
+    {
+        HashSet<string> temp = new();
+#if UNITY_2019_3_OR_NEWER
+        var fieldInfoGetterMethod = typeof(Editor).Assembly.GetType("UnityEditor.ScriptAttributeUtility")
+            .GetMethod("GetFieldInfoAndStaticTypeFromProperty",
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+#else
+    var fieldInfoGetterMethod =
+ typeof( Editor ).Assembly.GetType( "UnityEditor.ScriptAttributeUtility" ) 
+.GetMethod( "GetFieldInfoFromProperty", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static );
+#endif
+        var fieldInfoGetter = (FieldInfoGetter)Delegate.CreateDelegate(typeof(FieldInfoGetter), fieldInfoGetterMethod);
+
+        var propertyAll = serializedObject.GetIterator();
+        var propertyVisible = serializedObject.GetIterator();
+        if (propertyAll.Next(true))
+        {
+            bool iteratingVisible = propertyVisible.NextVisible(true);
+            do
+            {
+                bool isVisible = iteratingVisible && SerializedProperty.EqualContents(propertyAll, propertyVisible);
+                if (isVisible)
+                    iteratingVisible =
+                        propertyVisible
+                            .NextVisible(false); // Change it to true if you want to enter child properties, as well
+                else
+                {
+                    Type propFieldType;
+
+                    // Internal Unity variables don't seem to have a FieldInfo but when SerializedProperty.type is "Array", we must consider it
+                    // visible to avoid false negatives because even though "Array" type doesn't have a FieldInfo, it can be a visible array property
+                    isVisible = propertyAll.type == "Array" || fieldInfoGetter(propertyAll, out propFieldType) != null;
+                }
+
+                if (isVisible && !PUBLIC_PROPERTIES_TO_IGNORE.Contains(propertyAll.name))
+                {
+                    temp.Add(propertyAll.name);
+                }
+            } while (propertyAll.Next(false)); // Change it to true if you want to enter child properties, as well
+        }
+
+        return temp;
+    }
+}
